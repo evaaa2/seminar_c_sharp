7e82367783836e74239fab7c7e3252896d3a7f72
Dekastello Eva <dekastello@gymvod.local>
2024-09-13 09:33:08 +0200

    first project comit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 44;
+            int y = 789;
+            long z = x * y;
+
+            Console.WriteLine(z);
+            Console.WriteLine("Hello world!");
+            Console.ReadKey();
+        }
+    }
+}
