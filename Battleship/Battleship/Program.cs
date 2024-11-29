be952a925a47c74cfa54351b083122f47fa968b4
Dekastello Eva <dekastello@gymvod.local>
2024-11-29 09:34:58 +0100

    battle 2

--- Battleship/Battleship/Program.cs
+++ Battleship/Battleship/Program.cs
@@ -10,7 +10,12 @@ namespace Battleship
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(".");
+            int[,] playerField = new int[10, 10];
+            int[,] computerField = new int[10, 10];
+            for (int i = 0; i < playerField.GetLength(1); i++)
+            {
+
+            }
             Console.ReadKey();
         }
     }
