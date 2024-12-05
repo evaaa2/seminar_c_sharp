fb9ad8a8a06ca460524ac4df08375feb909b4bf4
Eva Dekastello <eva.dekastello@email.cz>
2024-12-05 21:47:15 +0100

    creating Battleship

--- Battleship/Battleship/Program.cs
+++ Battleship/Battleship/Program.cs
@@ -8,14 +8,55 @@ namespace Battleship
 {
     internal class Program
     {
+        static void PrintArray(string[,] arrayToPrint, string[] letters)
+        {
+            
+            for (int i = 0; i <= arrayToPrint.GetLength(1); i++)
+            {
+                Console.Write(letters[i] + " ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < arrayToPrint.GetLength(0); i++)
+            {
+                Console.Write(i+1 + " ");
+                for (int j = 0; j < arrayToPrint.GetLength(1); j++)
+                {
+                    Console.Write(arrayToPrint[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    
+        static void FillArray(string[,] arrayToFill)
+        {
+            for (int i = 0; i < arrayToFill.GetLength(0); i++)
+            {
+                for (int j = 0; j < arrayToFill.GetLength(1); j++)
+                {
+                    arrayToFill[i, j] = "~";
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void NewShip()
+        {
+
+        }
         static void Main(string[] args)
         {
-            int[,] playerField = new int[10, 10];
-            int[,] computerField = new int[10, 10];
-            for (int i = 0; i < playerField.GetLength(1); i++)
-            {
+            // definovani promennych
+            string[,] playerField = new string[10, 10];
+            string[,] computerField = new string[10, 10];
+            string[] letters = {"*", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" };
+            // uvod do hry
+            Console.WriteLine("Vitej ve hre Battleship!");
+            Console.WriteLine("Toto je tve pole, do ktereho umistis sve lode");
+            FillArray(playerField);
+            PrintArray(playerField, letters);
+            //umistovani lode do hraciho pole
 
-            }
+            
             Console.ReadKey();
         }
     }
