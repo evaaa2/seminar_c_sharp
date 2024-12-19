a50618c2b5b050db4f1a0b728ee002b64af25a6c
Eva Dekastello <eva.dekastello@email.cz>
2024-12-19 16:12:12 +0100

    controlling input

--- Battleship/Battleship/Program.cs
+++ Battleship/Battleship/Program.cs
@@ -6,16 +6,18 @@ using System.Threading.Tasks;
 
 namespace Battleship
 {
+    /*
     public class Monster
     {
         public string Name { get; set; }
         public int Level { get; set; }
         public int Health { get; set; }
     }
+    */
 
     internal class Program
     {
-        static void PrintArray(string[,] arrayToPrint, string[] letters)
+        static void PrintArray(string[,] arrayToPrint, List<string> letters)
         {
             
             for (int i = 0; i <= arrayToPrint.GetLength(1); i++)
@@ -25,7 +27,7 @@ namespace Battleship
             Console.WriteLine();
             for (int i = 0; i < arrayToPrint.GetLength(0); i++)
             {
-                Console.Write(i+1 + " ");
+                Console.Write(i + " ");
                 for (int j = 0; j < arrayToPrint.GetLength(1); j++)
                 {
                     Console.Write(arrayToPrint[i, j] + " ");
@@ -36,34 +38,93 @@ namespace Battleship
     
         static void FillArray(string[,] arrayToFill)
         {
+            Console.WriteLine();
             for (int i = 0; i < arrayToFill.GetLength(0); i++)
             {
                 for (int j = 0; j < arrayToFill.GetLength(1); j++)
                 {
                     arrayToFill[i, j] = "~";
                 }
-                Console.WriteLine();
             }
         }
+        static bool CheckMyCell(string letterCoordinate, int numberCoordinate, List<string> letters, string[,] myField)
+        {
+            if (
+                letters.Contains(letterCoordinate) &&
+                0 <= numberCoordinate && numberCoordinate < 10 &&
+                myField[numberCoordinate, letters.IndexOf(letterCoordinate) - 1] == "~"
 
+                )
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("spatny vstup");
+                return true;
+            }
+        }
         static void NewShip()
         {
 
+        }
+        static bool CheckTheShip()
+        {
+
+            return true;
         }
         static void Main(string[] args)
         {
             // definovani promennych
             string[,] playerField = new string[10, 10];
             string[,] computerField = new string[10, 10];
-            Dictionary<string, string> ships = new Dictionary<string, string>();
+            //definovani lodi
+            Dictionary<string, int> ships = new Dictionary<string, int>() 
+            {
+               {"L", 5},
+               {"B", 4},
+               {"K", 3},
+               {"P", 3},
+               {"T", 2},
+            };
+            //retezec stringu pro vypsani tabulky
+            List<string> letters = new List<string>
+            {"*", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J",};
 
-            string[] letters = {"*", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" };
             // uvod do hry
             Console.WriteLine("Vitej ve hre Battleship!");
             Console.WriteLine("Toto je tve pole, do ktereho umistis sve lode");
             FillArray(playerField);
             PrintArray(playerField, letters);
             //umistovani lode do hraciho pole
+            Console.WriteLine();
+            Console.WriteLine("Nyni umisti sve lode");
+            Console.WriteLine("K dispozici mas tyto lode:\n1x Letadlova lod (1 x 5)\n1x Bitevni lod (1 x 4)\n1x Kriznik (1 x 3)\n1x Ponorka (1 x 3)\n1x Torpedoborec (1 x 2)\n");
+            Console.WriteLine("Vzdy zadej pocatecni a koncovou souradnici lode. Pozor at ma pozadovanou delku, je v poli a neprekryva se s zadnou jinou z lodi!");
+            //Letadlova lod
+            string letterCoordinate;
+            int numberCoordinate = -1;
+            foreach(KeyValuePair<string, int> kvp in ships)
+            {
+                Console.WriteLine("Dalsi lod");
+                do
+                {
+                    Console.WriteLine("pocatecni souradnice");
+                    string coordinate = Console.ReadLine();
+                    letterCoordinate = Convert.ToString(coordinate[0]);
+                    if (!int.TryParse(Convert.ToString(coordinate[1]), out numberCoordinate))
+                    {
+                        Console.WriteLine("spatny vstup");
+                    }
+                    
+                    playerField[numberCoordinate, letters.IndexOf(letterCoordinate) - 1] = kvp.Key;
+                } while (CheckMyCell(letterCoordinate, numberCoordinate, letters, playerField));
+                break;
+            }
+            PrintArray(playerField, letters);
+
+            
+        /*
             //pokus pouziti tridy z chatu GPT
              List<Monster> monsters = new List<Monster>
         {
@@ -77,6 +138,7 @@ namespace Battleship
                 {
                     Console.WriteLine($"Name: {monster.Name}, Level: {monster.Level}, Health: {monster.Health}");
                 }
+        */
             
         
 
