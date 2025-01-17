841f63f4dd49b529f6d5c621a0af5e3c5f87233e
Dekastello Eva <dekastello@gymvod.local>
2025-01-17 09:38:00 +0100

    playground rectangle

---
+++ 17 - 17.1.2025/ClassPlayground/ClassPlayground/BankAccount.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPlayground
+{
+    /*
+ * 2) Vytvoř třídu BankAccount, kterou budeme reprezentovat bankovní účet
+ *    Přidej třídě BankAccount čtyři proměnné - accountNumber jako číslo účtu
+ *                                            - holderName jako jméno osoby, které účet patří
+ *                                            - currency jako měna, ve které je účet vedený
+ *                                            - balance jako zůstatek na účtu
+ *    Přidej třídě BankAccount čtyři funkce - Deposit, která jako vstupní parametr přijme množství peněz a vloží je na účet
+ *                                          - Withdraw, která jako vstupní parametr přijme množství peněz a z účtu "vybere" peníze, tedy sníží zůstatek a navrátí požadované množství
+ *                                                      Pokud na účtu není dostatek peněz, uživatele upozorní a vrátí nulu.
+ *                                          - Transfer, která jako vstupní parametry přijme množství peněz, účet ze kterého půjdou peníze, a účet na který peníze přijdou
+ *                                                      Opět kontroluj, jestli je na účtu, ze kterého převod jde, dostatek peněz
+ *                                                      (Můžeš do funkce přidat i to, že uživateli vypíše obě čísla účtu a uživatel musí třeba mezerníkem potvrdit, že jsou čísla účtu správná.)
+ *    Přidej třídě BankAccount konstruktor, který bude přijímat dva parametry - jméno majitele účtu a měnu, ve které bude účet vedený
+ *                                                                            - Při jeho zavolání nastav jméno a měnu podle vstupních parametrů, accountNumber nastav jako náhodně
+ *                                                                              vygenerované číslo velké alespoň 100 000 000 a menší, než 10 000 000 000 a balance nastav na nulu
+ * 
+ * 2) BONUS - Až vytvoříš BankAccount, přidej varianty funkcí výběru, vkladu a převodu s měnou jako vstupním parametrem, tedy pokud měna při vkladu/výběru (nebo měna účtu, na který se převádí)
+ *            je odlišná od měny, ve které je účet veden, zohledni to a správně vynásob peníze kurzem, který najdeš na internetu. Pro uložení kurzů si můžeš udělat public statické proměnné
+ *            ve třídě BankAccount, ke kterým se budeš odkazovat např. "int transactionRate = BankAccount.eurToCzk", kde eurToCzk je public static integer
+ */
+    internal class BankAccount
+    {
+        private int accountNumber;
+        private string holderName;
+        private string currency;
+        private int balance;
+
+        public BankAccount(string holderName, string currency)
+        {
+            this.holderName = holderName;
+            this.currency = currency;
+
+        }
+
+        public void Deposit()
+        {
+
+        }
+        public void WithDraw()
+        {
+
+        }
+        public void Transfer()
+        {
+
+        }
+
+        private int GetAccountNumber()
+        {
+        
+        }
+    }
+}
---
+++ 17 - 17.1.2025/ClassPlayground/ClassPlayground/Rectangle.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPlayground
+{
+    internal class Rectangle
+    {
+        private float width;
+        private float height;
+        public float area;
+        public float aspectRatio;
+
+        public Rectangle(float widthInput, float heightInput)
+        {
+            
+            width = widthInput;
+            height = heightInput;
+        }
+        public float CalculateArea()
+        {
+            area = width * height;
+            return area;
+        }
+        public float CalculateAspectRatio()
+        {
+            aspectRatio = width / height;
+            return aspectRatio;
+        }
+        public bool ContainsPoint(float x, float y)
+        {
+            if (x <=0 || y <=0 || x >=width || y >=height) return false;
+            else return true;
+        }
+        
+    }
+}
