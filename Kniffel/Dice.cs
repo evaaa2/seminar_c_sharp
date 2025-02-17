7762eff54bb115db0be2917a8cdff26178a06704
Eva Dekastello <eva.dekastello@email.cz>
2025-02-17 18:29:58 +0100

    creating project

---
+++ Kniffel/Dice.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kniffel
+{
+    internal class Dice
+    {
+        public Dice() { 
+        
+        }
+
+        public void Throw()
+        {
+
+        }
+    }
+}
---
+++ Kniffel/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kniffel
+{
+    internal class Program
+    {
+        static void Intro()
+        {
+            Console.WriteLine("WELCOME TO KNIFFEL!");
+            Console.WriteLine("(Also known as Yatzy)");
+            Console.WriteLine();
+            Console.ReadKey();
+            bool wrongInput = true;
+            while (wrongInput)
+            {
+                Console.WriteLine("Do you know the rules of this game? (Write yes or no)");
+                string answer = Console.ReadLine();
+                if (answer == "yes" || answer == "Yes" || answer == "YES")
+                {
+                    Console.WriteLine("Alright then, let's start the game!");
+                    wrongInput = false;
+                }
+
+                else if (answer == "no" || answer == "No" || answer == "NO")
+                {
+                    ExplainTheRules();
+                    wrongInput = false;
+                }
+
+                else Console.WriteLine("Wrong input!");
+                Console.WriteLine();
+            }
+        }
+
+        static void ExplainTheRules()
+        {
+            //will be done later on
+            Console.WriteLine("Well, then look them up on the internet!");
+        }
+
+        static void Dice()
+        {
+            Console.WriteLine("Your dice:");
+            Console.WriteLine("  +++");
+            Console.WriteLine("  +?+");
+            Console.WriteLine("  +++");
+
+            Console.WriteLine("To throw the dice, press any key.");
+            Console.ReadKey();
+
+
+
+
+        }
+
+        
+
+        static void Main(string[] args)
+        {
+            Dice classic = new Dice();
+
+            Intro();
+            Console.ReadKey();
+
+            Console.Clear();
+            Dice();
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
